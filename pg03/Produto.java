e5f2fd62e70c8e1eb62b52235cb65339df7d191f
rodrygocb <71535965+rodrygocb@users.noreply.github.com>
2023-05-29 09:01:58 -0300

    Create Produto.java

---
+++ pg03/Produto.java
@@ -0,0 +1,13 @@
+package pa03;
+
+public class Produto {
+    String nome;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+        }
+    
+    public String getNome() {
+        return this.nome;
+        }    
+}
