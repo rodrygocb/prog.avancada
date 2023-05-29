07588927c746bdf45dc5bf3c35736995876f1e9b
rodrygocb <71535965+rodrygocb@users.noreply.github.com>
2023-05-29 09:01:14 -0300

    Create Itemcartaz.java

---
+++ pg03/Itemcartaz.java
@@ -0,0 +1,23 @@
+package pa03;
+
+
+public class ItemCartaz {
+    double preco; 
+    Produto produto;
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+        }
+    
+    public Produto getProduto() {
+        return this.produto;
+        }  
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+        }
+        
+    public Double getPreco() {
+        return this.preco;
+        }  
+}
