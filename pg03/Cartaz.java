ea5dd3d869f083ed7662ecc02c9db392f56def87
rodrygocb <71535965+rodrygocb@users.noreply.github.com>
2023-05-29 09:00:49 -0300

    Create Cartaz.java

---
+++ pg03/Cartaz.java
@@ -0,0 +1,23 @@
+package pa03;
+
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Cartaz {
+    
+
+    /**
+     *
+     */
+    List itensCartaz = new ArrayList();
+    
+    public void additem(ItemCartaz ic){
+        itensCartaz.add(ic);
+    }
+
+    public List getItens(){
+        return this.itensCartaz;
+    }
+}
+ 
