d184433c9e169b072b488c54b6f3ec446e8406a9
rodrygocb <71535965+rodrygocb@users.noreply.github.com>
2023-05-29 09:01:37 -0300

    Create Main.java

---
+++ pg03/Main.java
@@ -0,0 +1,41 @@
+package pa03;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Produto p1 = new Produto();
+        p1.setNome("feijão");
+    
+        Produto p2 = new Produto();
+        p2.setNome("arroz");
+    
+        Cartaz cartaz = new Cartaz();
+    
+        ItemCartaz ic1 = new ItemCartaz();
+        ic1.setProduto(p1);
+        ic1.setPreco(5.0);
+
+        ItemCartaz ic2 = new ItemCartaz();
+        ic2.setProduto(p2);
+        ic2.setPreco(4.5);
+
+        cartaz.additem(ic1);
+        cartaz.additem(ic2);
+
+    
+        System.out.println("Produto | Preço");
+
+        List itens = cartaz.getItens();
+        for (int i = 0; i < itens.size(); i++){
+            ItemCartaz ic = (ItemCartaz)itens.get(i);
+            
+            System.out.print(ic.getProduto().getNome()+" | ");
+            System.out.println(ic.getPreco());
+        }
+    }
+
+    
+}
+
